fd428e238cac723879048a6de2b56cdc0a4c3a2a
mergwyn <gary@theclarkhome.com>
2023-09-13 11:14:48 +0100

    validate_ functions are deprecated comment out for now

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,37 +8,37 @@
 #
 class managedmac {
 
-  if $::osfamily != 'Darwin' {
-    fail("unsupported osfamily: ${::osfamily}")
+  if $facts['os']['family'] != 'Darwin' {
+    fail("unsupported osfamily: ${facts['os']['family']}")
   }
 
-  $min_os_version = '10.9'
+  $min_os_version = '11.0'
 
-  if version_compare($::macosx_productversion_major, $min_os_version) < 0 {
-    fail("unsupported product version: ${::macosx_productversion_major}")
+  if version_compare($facts['os']['version']['major'], $min_os_version) < 0 {
+    fail("unsupported product version: ${facts['os']['version']['major']}")
   }
 
   include managedmac::ntp
   include managedmac::activedirectory
-  include managedmac::security
+  #include managedmac::security
   include managedmac::desktop
   include managedmac::mcx
   include managedmac::filevault
   include managedmac::loginwindow
   include managedmac::softwareupdate
   include managedmac::energysaver
-  include managedmac::portablehomes
-  include managedmac::mounts
+  #include managedmac::portablehomes
+  #include managedmac::mounts
   include managedmac::loginhook
   include managedmac::logouthook
-  include managedmac::sshd
-  include managedmac::remotemanagement
-  include managedmac::screensharing
-  include managedmac::mobileconfigs
-  include managedmac::propertylists
+  #include managedmac::sshd
+  #include managedmac::remotemanagement
+  #include managedmac::screensharing
+  #include managedmac::mobileconfigs
+  #include managedmac::propertylists
   include managedmac::execs
   include managedmac::files
-  include managedmac::users
+  #include managedmac::users
   include managedmac::groups
   include managedmac::cron
 
